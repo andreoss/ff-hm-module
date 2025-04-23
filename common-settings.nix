856f8d57479dfbfe10dd4eda067859644694fb94
andreoss <andreoss@sdf.org>
2025-04-23 01:06:11 -0400

    ui

--- common-settings.nix
+++ common-settings.nix
@@ -1,5 +1,7 @@
 { config, ... }:
 {
+  "browser.startup.homepage" = "about:blank";
+
   "security.cert_pinning.enforcement_level" = 1;
   "accessibility.force_disabled" = 1;
   "browser.cache.disk.enable" = false;
@@ -7,7 +9,6 @@
   "browser.link.open_newwindow" = 2;
   "browser.privatebrowsing.autostart" = true;
   "browser.search.searchEnginesURL" = "";
-  "browser.startup.homepage" = "about:blank";
   "browser.tabs.tabMinWidth" = 16;
   "browser.uidensity" = 1;
   "extensions.abuseReport.enabled" = false;
@@ -24,4 +25,43 @@
   "font.name.sans-serif.x-western" = "Terminus";
   "font.name.serif.x-western" = "Terminus";
   "browser.translations.panelShown" = false;
+  "browser.uiCustomization.state" = builtins.toJSON {
+    currentVersion = 20;
+    newElementCount = 5;
+    dirtyAreaCache = [
+      "nav-bar"
+      "PersonalToolbar"
+      "toolbar-menubar"
+      "TabsToolbar"
+      "widget-overflow-fixed-list"
+    ];
+    placements = {
+      PersonalToolbar = [ "personal-bookmarks" ];
+      TabsToolbar = [
+        "tabbrowser-tabs"
+        "new-tab-button"
+        "alltabs-button"
+      ];
+      nav-bar = [
+        "back-button"
+        "forward-button"
+        "stop-reload-button"
+        "urlbar-container"
+        "downloads-button"
+        "ublock0_raymondhill_net-browser-action"
+        "_testpilot-containers-browser-action"
+        "reset-pbm-toolbar-button"
+        "unified-extensions-button"
+      ];
+      toolbar-menubar = [ "menubar-items" ];
+      unified-extensions-area = [ ];
+      widget-overflow-fixed-list = [ ];
+    };
+    seen = [
+      "save-to-pocket-button"
+      "developer-button"
+      "ublock0_raymondhill_net-browser-action"
+      "_testpilot-containers-browser-action"
+    ];
+  };
 }
